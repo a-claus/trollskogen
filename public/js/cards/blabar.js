2a0291c8282e065078cca5fa0bd4f3961993dd75
allan <pictjur@gmail.com>
2020-02-03 17:22:34 +0100

    Update blabar.js

--- public/js/cards/blabar.js
+++ public/js/cards/blabar.js
@@ -26,11 +26,33 @@ kartObj.push({
     namn: "Blåbar",
     img: mapImages[mapImages.length-1],
     miljo: true, figur : false, info: false, floor:1,
-    x: -1, y: -1,
+    x:-1, y:-1,
   draw: function(){
-      if (this.x == -1) getPosition();
-        ctx.drawImage(this.img, 175, 175, 50 ,50);//, 100, 100
-    }
+      if (this.x == -1) {this.getPosition();}
+        ctx.drawImage(this.img, this.x, this.y, 50 ,50);//, 100, 100
+  },
+  getPosition: function(){
+    let x = 200; let y = 200; let color; let i = 0;
+    let incX; let incY;
+    incX = Math.floor(Math.random() * 4) * 2 - 3; //0 2 4 6
+    incY = Math.floor(Math.random() * 4) * 2 - 3;
+      while (i < 2){
+        x += incX;
+        y += incY; 
+        color = getRGB(x, y);
+
+        if (color == "255 255 255") {i = 1};
+        if (i == 1){
+          if (color != "255 255 255"){
+            i = 2;
+          }
+        }
+
+      }
+    this.x = x - 25; this.y = y - 25;
+    gameObj[this.go].hitAreaX = this.x - 11; gameObj[this.go].hitAreaY = this.y -7;
+  },
+  go: gameObj.length
 });
 
 
@@ -83,25 +105,7 @@ drawRuta: function(){
 
 	});
 
-function getPosition(){
-  let x; let y; let color; let i = 0;
-  let incX; let incY;
-  incX = Math.floor(Math.random()*2);
-  incY = Math.floor(Math.random()*2);
-   while (i < 2){
-    x += incX;
-    y += incY; 
-    color = getRGB(x, y);
 
-    if (color == "255 255 255") i = 1;
-    if (i == 1){
-      if (color != "255 255 255"){
-        i=2;
-      }
-    }
-
-   }
-}
 
 
 console.log("Blåbär klar");
